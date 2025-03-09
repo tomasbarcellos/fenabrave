49e958e31c518a8ebbee9f7ba02673b8c7f254c9
Tomas de Siervi Barcellos <tomasbarcellos@gmail.com>
2025-03-09 00:00:36 -0300

    Documento tabela

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Quantidade de veiculos emplacados, por ano e modelo
+#'
+#' Um conjunto de dados que contem os 50 principais modelos de automoveis e
+#'  comerciais leves de cada ano (2003-2024).
+#'  As variaveis são as seguintes:
+#'
+#' @format Uma tibble com 2200 linhas and 5 variáveis:
+#' \describe{
+#'   \item{tipo}{Se automovel ou utilitário}
+#'   \item{montadora}{Montadora que produz o modelo}
+#'   \item{modelo}{Nome do modelo}
+#'   \item{quantidade}{Quantidade de veiculos emplacados no ano}
+#'   \item{ano}{Ano do emplacamento}
+#' }
+"emplacamento_anual"
