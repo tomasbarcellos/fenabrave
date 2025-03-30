9132bb68ab5e1a753d003964bc794be13b26d320
Tomas de Siervi Barcellos <tomasbarcellos@gmail.com>
2025-03-29 23:21:46 -0300

    Inclui passo a passo para pegar preco do carro

--- R/tabelaFipe.R
+++ R/tabelaFipe.R
@@ -55,33 +55,57 @@ get_modelos <- function(referencia, cod_marca) {
         purrr::map_chr(2),
     )
   )
-
-
 }
-tabela_fipe <- function(referencia, marca, modelo, ano) {
+
+
+tabela_fipe <- function(ano, marca, modelo) {
   referencia <- get_referencia(ano)
+
+  rg_marca <- stringr::regex(marca, ignore_case = TRUE)
+
   cod_marca <- get_marcas(referencia) %>%
-    dplyr::filter(marcas == marca) %>%
+    dplyr::filter(stringr::str_detect(marcas, rg_marca)) %>%
     dplyr::pull(codigo)
 
   tbl_modelos <- get_modelos(referencia, cod_marca)
 
-  link2 <- "https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros/"
-  partes <- stringr::str_c("codigoTabelaReferencia={referencia}&",
-                           "codigoMarca={marca}&codigoModelo={modelo}&",
-                           "codigoTipoVeiculo=1&anoModelo={ano}&",
-                           "codigoTipoCombustivel=1&tipoVeiculo=carro&",
-                           "modeloCodigoExterno=&tipoConsulta=tradicional") %>%
-    stringr::str_split_1("&") %>%
-    stringr::str_split("=")
-  body <- partes %>%
-    purrr::map(2)
-  names(body) <- partes %>%
-    purrr::map_chr(1)
+  rg_busca <- stringr::regex(modelo, ignore_case = TRUE)
 
-  rpost <- httr::POST(link2, body = body, encode = "json")
-  rpost %>% httr::content()
+  cod_modelo <- tbl_modelos$modelos %>%
+    dplyr::filter(stringr::str_detect(modelos, rg_busca)) %>%
+    # ta quebrando
+    # head(1) %>%
+    dplyr::pull(codigos)
+
+  link2 <- "https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros/"
+  links <- glue::glue("codigoTabelaReferencia={referencia}&",
+                           "codigoMarca={cod_marca}&codigoModelo={cod_modelo}&",
+                           "codigoTipoVeiculo=1&anoModelo=32000&",
+                           "codigoTipoCombustivel=1&tipoVeiculo=carro&",
+                      "modeloCodigoExterno=&tipoConsulta=tradicional")
+
+  pegar_preco <- function(link) {
+    partes <- link %>%
+      stringr::str_split_1("&") %>%
+      stringr::str_split("=")
+    body <- partes %>%
+      purrr::map(2)
+    names(body) <- partes %>%
+      purrr::map_chr(1)
+
+    if (sample(c(TRUE, FALSE), 1, prob = c(0.3, 0.7))) {
+      Sys.sleep(abs(rnorm(1, 1, 0.5)))
+    }
+
+    httr::POST(link2, body = body, encode = "json") %>%
+      httr::content() %>%
+      tibble::as_tibble()
+  }
+  purrr::map(links, purrr::safely(pegar_preco))
 }
 
-
+gol <- tabela_fipe(2005, "volkswagen", "gol")
+gol %>%
+  purrr::map_df("result") %>%
+  dplyr::filter(!is.na(Valor))
 
